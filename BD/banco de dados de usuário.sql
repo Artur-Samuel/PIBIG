26380f8e2ed8acac440f52207fa9b6192a89f68e
Artur-Samuel <arturrsamuel1@gmail.com>
2026-03-05 20:43:26 -0300

    Add files via upload

--- BD/banco de dados de usuário.sql
+++ BD/banco de dados de usuário.sql
@@ -9,10 +9,8 @@ CREATE TABLE usuarios (
 );
 ALTER TABLE usuarios ADD COLUMN role ENUM('usuario','admin') NOT NULL DEFAULT 'usuario';
 ALTER TABLE usuarios ADD foto VARCHAR(255) NULL;
-INSERT INTO usuarios (nome, email, senha, role) VALUES ('Artur Samuel', 'arturrsamuel1@gmail.com', '$2y$10$cFzfDqttUlc8qF6zZVApHeCvKzTOlfnJRVJVwatrTjksanifAs0Jq', 'admin');
-INSERT INTO usuarios (nome, email, senha, role) VALUES ('Snake Sabotage', 'snakesabotage@gmail.com', 'arturamo', 'admin');
-
-
+INSERT INTO usuarios (nome, email, senha, role) VALUES ('Ulisses Roque Tomaz', 'ulisses@gmail.com', '$2y$10$SsKavSelFefe.qkmG227cuczxxEbjHEFX.97VgWb5ccogg9f7YX6u', 'admin');
+-- Senha: ulissestomaz --
 
 select*from usuarios;
 
@@ -24,5 +22,19 @@ CREATE TABLE eventos (
     criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+ALTER TABLE eventos ADD imagem VARCHAR(255) NULL;
 select*from eventos;
+
+CREATE TABLE escala_multimidia (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    data_servico DATE NOT NULL,
+    funcao VARCHAR(50) NOT NULL,
+    usuario_id INT NOT NULL,
+    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE
+);
+
+ALTER TABLE escala_multimidia 
+ADD turno VARCHAR(20) NOT NULL AFTER data_servico;
+select*from escala_multimidia;
 
